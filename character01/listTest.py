b5a55065847c399cac9622e233f7b7044135e4b5
suzy56 <1096315120@qq.com>
2018-07-01 12:23:15 +0800

    list test

---
+++ character01/listTest.py
@@ -0,0 +1,35 @@
+# list
+box=['red_ball','white_ball','bule_ball'];
+
+print(len(box));
+
+print(box[1]);
+
+# 打印box中倒数第一个元素
+print(box[-1]);
+
+# 未指定index默认删除list末尾的元素
+box.pop();
+
+# 追加元素到末尾
+box.append('pink_ball');
+
+print(box[-1]);
+
+# 指定位置插入元素
+
+box.insert(0,'black_ball');
+
+print(box);
+
+# 对象
+testJson={'kk':2};
+
+print(testJson['kk']);
+
+# tuple 有序列表-元组
+
+constant_box = (2,3,4);
+print(constant_box);
+
+
